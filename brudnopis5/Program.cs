e1ca825715b8a444bbd7f32f4bb3eace25e2e61c
mariusz <mariusz@MARIUSZ-I7>
2018-11-05 12:25:38 +0100

    Add project files.

---
+++ brudnopis5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brudnopis5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataOd = new DateTime(2018, 10, 30);
+            DateTime dataDo = new DateTime(2018, 11, 5);
+
+            while (dataOd < dataDo)
+            {
+                Console.WriteLine(dataOd);
+                dataOd = dataOd.AddDays(1);
+            }
+            
+            Console.ReadKey();
+        }
+
+        
+    }
+}
